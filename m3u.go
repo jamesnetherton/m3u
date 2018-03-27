b00ecbb9c90767946b699d1241719fb763b44ef3
Soukron <soukron@gmbros.net>
2018-03-27 10:48:37 +0200

    Replaced os.Open with openuri.Open to support remote locations
    * Replaced os.Open with openuri.Open to support remote locations
    
    * Implemented logic to use http.Get or os.Open

--- m3u.go
+++ m3u.go
@@ -3,6 +3,8 @@ package m3u
 import (
 	"bufio"
 	"errors"
+	"io"
+	"net/http"
 	"os"
 	"regexp"
 	"strconv"
@@ -30,7 +32,15 @@ type Track struct {
 
 // Parse parses an m3u playlist with the given file name and returns a Playlist
 func Parse(fileName string) (playlist Playlist, err error) {
-	f, err := os.Open(fileName)
+	var f io.ReadCloser
+	var data *http.Response
+	if strings.HasPrefix(fileName, "http://") || strings.HasPrefix(fileName, "https://") {
+		data, err = http.Get(fileName)
+		f = data.Body
+	} else {
+		f, err = os.Open(fileName)
+	}
+	
 	if err != nil {
 		err = errors.New("Unable to open playlist file")
 		return
--- m3u_test.go
+++ m3u_test.go
@@ -35,6 +35,36 @@ func TestPlaylist(t *testing.T) {
 	}
 }
 
+func TestRemotePlaylist(t *testing.T) {
+	playlist, _ := Parse("https://raw.githubusercontent.com/jamesnetherton/m3u/master/testdata/playlist.m3u")
+
+	if len(playlist.Tracks) != 5 {
+		t.Fatalf("Expected track count to be 5")
+	}
+
+	for i := 0; i < 5; i++ {
+		if playlist.Tracks[i].Length != i+1 {
+			t.Fatalf("Expected track Length to be %d but was %d", i+1, playlist.Tracks[i].Length)
+		}
+
+		if playlist.Tracks[i].Name != fmt.Sprintf("Track %d", i+1) {
+			t.Fatalf("Expected track name to be Track %d but was '%s'", i+1, playlist.Tracks[i].Name)
+		}
+
+		if playlist.Tracks[i].URI != fmt.Sprintf("Track%d.mp4", i+1) {
+			t.Fatalf("Expected track URI to be Track%d.mp4 but was '%s'", i+1, playlist.Tracks[i].URI)
+		}
+
+                if playlist.Tracks[i].Tags[0].Name != "group-title" {
+			t.Fatalf("Expected tag to be group-title but was '%s'", playlist.Tracks[i].Tags[0].Name)
+                }
+
+                if playlist.Tracks[i].Tags[0].Value != "Album1" {
+			t.Fatalf("Expected group-title tag value to be Album1 but was '%s'", playlist.Tracks[i].Tags[0].Value)
+                }
+	}
+}
+
 func TestPlaylistInvalidHeader(t *testing.T) {
 	_, err := Parse("testdata/playlist_no_header.m3u")
 	if err == nil {
